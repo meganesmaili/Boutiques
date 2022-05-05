684fb6348ff257a23c3819231e5ff6f5bff9589f
Megane SMAILI <megane.smaili.dev@gmail.com>
2022-05-05 11:39:51 +0200

    first push boutiques

---
+++ categorie.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- JavaScript Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <link rel="stylesheet" href="style.css">
+    <script src="scripts.js" defer></script>
+        <title>Boutiques.</title>
+    </head>
+    <body>
+        <header class="colorBack">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col titlePosition">
+                        <a href="#" title="Shop" class="titleColor">Boutique.</a>
+                    </div>
+                    <div class="spinner-category">
+                    </div>
+                <nav class="my-3 ">
+                    <ul class="nav justify-content-center">
+                            <li class="nav-item">
+                            <a class="nav-link menuTextColor" aria-current="page" href="../pagesAccueil/index.php">Home</a>
+                            </li>
+                    </ul>
+                </nav>
+                </div>
+            </div>
+        </header>
---
+++ scripts.js
@@ -0,0 +1,110 @@
+fetch('https://fakestoreapi.com/products/categories')
+ .then(res=>res.json())
+ .then(categories=> {
+
+    const loader = document.querySelector('.spinner-border');
+    loader.classList.add('d-none');
+
+
+    const navCat = document.querySelector('.navCat');
+
+    categories.forEach(categorie => {
+        const link = document.createElement('a');
+        link.innerText = categorie;
+        link.href =`categorie.php?name=${categorie}`;
+        link.classList.add('nav-link', 'text-secondary');
+ 
+         //Ajoute la categorie dans le menu
+        navCat.appendChild(link);
+ 
+    });
+ 
+    
+})
+
+
+
+const createTable = (datas) => {
+
+    const body = document.querySelector('body');
+    let main = document.createElement('main');
+    let row = document.createElement('div');
+    row.classList.add('row');
+
+
+    
+    datas.forEach(object =>{
+
+    let sizeCard = document.createElement('div');
+    sizeCard.classList.add('col-lg-6', 'col-12');
+    let card = document.createElement('div');
+    card.classList.add('card');
+    
+        
+
+    let image = document.createElement('img');
+    image.classList.add('card-img-top','w-25')
+    image.src = object.image
+    image.alt = 'Vêtement'
+        
+    let globalCard = document.createElement('div');
+    globalCard.classList.add('card-body');
+        
+    let title = document.createElement('h5');
+    title.innerText = object.title
+    title.classList.add('card-title')
+
+    let price = document.createElement('p');
+    price.innerText = object.price
+    price.classList.add('card-text')
+
+    let description = document.createElement('p');
+    description.innerText = object.description
+    description.classList.add('card-text')
+
+    let categories = document.createElement('a');
+    categories.innerText = object.category
+    categories.classList.add('btn', 'btn-primary')
+
+        
+    card.appendChild(image);  
+
+    globalCard.appendChild(title);  
+
+    globalCard.appendChild(description);
+
+    globalCard.appendChild(price);
+        
+    globalCard.appendChild(categories);
+        
+    card.appendChild(globalCard);
+
+    sizeCard.appendChild(card);
+
+    row.appendChild(sizeCard);
+
+        
+    
+
+
+    });
+    
+    main.appendChild(row);
+    body.appendChild(main)
+}
+
+
+let allWearing = () => {
+
+    fetch('https://fakestoreapi.com/products?limit=16')
+        .then(response => response.json())
+        .then(datas => {
+    
+            createTable(datas);
+            
+        })
+        .catch(error => alert(error));
+    
+    }
+    
+    allWearing();
